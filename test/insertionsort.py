47b7f9e6f80017084e351d02cdbf1ed14baee7e2
MrYezhipeng <939647181@qq.com>
2020-02-25 12:59:55 +0800

    min stack

--- test/insertionsort.py
+++ test/insertionsort.py
@@ -7,5 +7,22 @@
 # @time: 2020-02-24 21:41
 # @desc:
 
+def mergesort(seq):
+    if len(seq) == 1:
+        return seq
 
-if __name__= '__main__':
+
+
+
+if __name__== '__main__':
+
+    test_seq = [1,32,41,2,1,42,12,43,123,53,2,5,62]
+
+    for index in range(len(test_seq)):
+        j = index
+        while test_seq[j-1] > test_seq[j] and j >=0:
+            test_seq[j-1],test_seq[j] = test_seq[j],test_seq[j-1]
+            j -= 1
+
+    print(test_seq)
+
